d107d8b44d69fd7edc04023ad2de6a29b4d64d88
Alyssa Madarang <aam235@njit.edu>
2020-04-05 15:53:44 -0400

    add updated UI prototype

--- UI/src/ChatUI.java
+++ UI/src/ChatUI.java
@@ -7,11 +7,14 @@ import java.awt.event.ActionListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class ChatUI {
+	// public SampleSocketClient client = new SampleSocketClient(); connect to Client
+	
 	public static void main(String[] args) {
 		//create frame
 		JFrame frame = new JFrame("Chat Room"); 
@@ -22,57 +25,122 @@ public class ChatUI {
 		chatRoom.setPreferredSize(new Dimension (400,400));
 		chatRoom.setLayout(new BorderLayout());
 		
-		//create text area for messages
-		JTextArea textArea = new JTextArea();
+		//create top panel
+		JPanel topConnect = new JPanel();
+		
+		
+		//create text area for messages and displaying users
+		JTextArea chatTextArea = new JTextArea();
+		JTextArea usersText = new JTextArea();
 		
 		//don't let the user edit this directly
-		textArea.setEditable(false);
-		textArea.setText("");
+		chatTextArea.setEditable(false);
+		chatTextArea.setText("");
 		
 		//create panel to hold multiple controls
 		JPanel chatArea = new JPanel();
+		chatArea.setPreferredSize(new Dimension(250, 400));
 		chatArea.setLayout(new BorderLayout());
 		
-		//add text area to chat area
-		chatArea.add(textArea, BorderLayout.CENTER);
+		//create panel to display connected users
+		JPanel usersArea = new JPanel();
+		usersArea.setPreferredSize(new Dimension(150, 400));
+		usersArea.setLayout(new BorderLayout());
+		
+		//add text area to chat area and usersArea
+		chatArea.add(chatTextArea, BorderLayout.CENTER);
 		chatArea.setBorder(BorderFactory.createLineBorder(Color.black));
 		
-		//add chat area to panel
-		chatRoom.add(chatArea, BorderLayout.CENTER);
+		usersArea.add(usersText, BorderLayout.CENTER);
+		usersArea.setBorder(BorderFactory.createLineBorder(Color.black));
+		
+		JLabel uLabel = new JLabel();
+		uLabel.setText("Online users");
+		
+		// add label to users area
+		usersArea.add(uLabel, BorderLayout.NORTH);
+		
+		
+		//add chat area and users area to panel
+		chatRoom.add(chatArea, BorderLayout.WEST);
+		chatRoom.add(usersArea, BorderLayout.EAST);
+		
+		//username textfield
+		JTextField usernameField = new JTextField("Your username");
+		usernameField.setPreferredSize(new Dimension(100, 30));
+		
+		
+		//IP and port input fields
+		JTextField ipAdd = new JTextField("IP address");
+		ipAdd.setPreferredSize(new Dimension(100, 30));
+		JTextField portNum = new JTextField("Port");
+		portNum.setPreferredSize(new Dimension(50, 30));
+		
+		//connect button
+		JButton connect = new JButton();
+		connect.setPreferredSize(new Dimension (100, 30));
+		connect.setText("Connect");
+		connect.addActionListener(new ActionListener() {
+		
+				
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					String username = usernameField.getText();
+					usersText.append("\n" + username);
+					
+					connect.setText("Reconnect");
+				
+				}
+			}
+		);
+		
+		
 		
 		//create panel to hold multiple controls
 		JPanel userInput = new JPanel();
 		
 		//setup textfield
-		JTextField textField = new JTextField();
-		textField.setPreferredSize(new Dimension(100, 30));
+		JTextField messageField = new JTextField();
+		messageField.setPreferredSize(new Dimension(100, 30));
+		
 		
 		//setup submit button
-		JButton b = new JButton();
-		b.setPreferredSize(new Dimension(100, 30));
-		b.setText("Send");
-		b.addActionListener(new ActionListener () {
+		JButton send = new JButton();
+		send.setPreferredSize(new Dimension(100, 30));
+		send.setText("Send");
+		send.addActionListener(new ActionListener () {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				String message = textField.getText();
+				String message = messageField.getText();
+				String username = usernameField.getText();
+		
+				
 				if(message.length()> 0) {
 					//append a new line & text from textfield to textarea
-					textArea.append("\n" + textField.getText());
+					chatTextArea.append("\n" + username + ": " + messageField.getText());
 					//reset textfield
-					textField.setText("");
+					messageField.setText("");
 				}
 			}
 			
 			
 		});
 		
-		//add textfield and button to panel
-		userInput.add(textField);
-		userInput.add(b);
+		//add username input and connect input 
+		topConnect.add(usernameField);
+		topConnect.add(ipAdd);
+		topConnect.add(portNum);
+		topConnect.add(connect);
 		
-		//add panel to chatRoom panel
+		//add textfield and button to panel
+		userInput.add(messageField);
+		userInput.add(send);
+		
+		//add panels to chatRoom panel
 		chatRoom.add(userInput, BorderLayout.SOUTH);
+		chatRoom.add(topConnect, BorderLayout.NORTH);
+		
 		
 		//add chatRoom panel to frame
 		frame.add(chatRoom, BorderLayout.CENTER);
